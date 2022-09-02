07eb084b8b45c6bd4ff64ed6561fa3916a4d6551
irack000 <IRACK000@gmail.com>
2022-09-02 10:43:17 +0900

    Init

---
+++ vscode/bin/remote-cli/code.cmd
@@ -0,0 +1,5 @@
+@echo off
+setlocal
+set ROOT_DIR=%~dp0..\..
+call "%ROOT_DIR%\node.exe" "%ROOT_DIR%\out\server-cli.js" "code" "1.71.0" "784b0177c56c607789f9638da7b6bf3230d47a8c" "code.cmd" %*
+endlocal
